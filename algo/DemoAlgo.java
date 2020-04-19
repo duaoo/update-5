43471ce2369f6f7624cf3926580e5450a4f766cc
duao <ideal.du@gmail.com>
2020-04-19 20:27:25 +0800

    Start here.

---
+++ algo/DemoAlgo.java
@@ -0,0 +1,11 @@
+/**
+ * @author duxiaojie
+ * @date 2020-04-19 8:24 下午
+ */
+public class DemoAlgo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello algo !");
+    }
+
+}
