e3bed521aa033255af8d0e95c2b430451d133e46
duao <ideal.du@gmail.com>
2020-05-18 23:55:27 +0800

    +L641

--- algo/stack/L42_Trap.java
+++ algo/stack/L42_Trap.java
@@ -1,7 +1,6 @@
 package stack;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Stack;
 
 /**
  * 接雨水
@@ -16,17 +15,33 @@ public class L42_Trap {
      */
     public int solution_1(int[] height) {
 
-        if (height == null || height.length == 0) return 0;
-        Deque<Integer> stack = new ArrayDeque<>();
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        Stack<Integer> stack = new Stack<>();
         int res = 0;
         for (int i = 0; i < height.length; i++) {
-            while (!stack.isEmpty() && height[stack.peek()] < height[i]) {
-                int tmp = stack.pop();
-                if (stack.isEmpty()) break;
-                res += (Math.min(height[i], height[stack.peek()]) - height[tmp]) * (i - stack.peek() - 1);
+
+            int nowHeight = height[i];
+
+            // 如果当前高度大于栈顶元素高度，则进行出栈面积计算
+            while (!stack.isEmpty() && height[stack.peek()] < nowHeight) {
+
+                // 栈顶元素出栈
+                int headIndex = stack.pop();
+                if (stack.isEmpty()) {
+                    break;
+                }
+
+                // 新栈顶元素
+                int newHeadIndex = stack.peek();
+
+                res += (Math.min(nowHeight, height[newHeadIndex]) - height[headIndex]) * (i - newHeadIndex - 1);
             }
             stack.push(i);
         }
+
         return res;
     }
 
