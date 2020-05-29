0d47d6def6cd70edb56615dad79fc609b8c289ce
duao <ideal.du@gmail.com>
2020-05-29 23:58:39 +0800

    +L242

--- algo/hash/L242_IsAnagram.java
+++ algo/hash/L242_IsAnagram.java
@@ -12,6 +12,8 @@ public class L242_IsAnagram {
 
     /**
      * 暴力求解
+     * 时间复杂度：O(nlogn)
+     * 空间复杂度：O(1)
      */
     public boolean solution_1(String s, String t) {
 
@@ -26,5 +28,34 @@ public class L242_IsAnagram {
         Arrays.sort(tChars);
 
         return Arrays.equals(sChars, tChars);
+    }
+
+    /**
+     * hash表
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     */
+    public boolean solution_2(String s, String t) {
+
+        // 长度不一致直接返回
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        // 遍历s，计算每个元素的自然序号为数组下标，对所在下标的数组值进行+1操作
+        int[] table = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i) - 'a']++;
+        }
+
+        // 遍历t，如果出现某个元素序号的值小于0，说明当前元素不存在s或比s元素出现次数多，则s与t不一致
+        for (int i = 0; i < t.length(); i++) {
+            if (--table[s.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
