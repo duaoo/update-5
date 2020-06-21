d4fa8293b1cf0b720c6642a8d853421964233b08
duao <ideal.du@gmail.com>
2020-06-21 23:50:15 +0800

    +L104

---
+++ algo/tree/L104_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,32 @@
+package tree;
+
+import bean.TreeNode;
+
+/**
+ * 二叉树的最大深度
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ *
+ * @author duxiaojie
+ */
+public class L104_MaximumDepthOfBinaryTree {
+
+
+    /**
+     * 左右节点递归
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     */
+    public int solution_1(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = solution_1(root.left);
+        int rightDepth = solution_1(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+
+    }
+
+}
